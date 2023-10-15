106926000144a884872379cd70e97bd75874bab7
Azani David Pinkney <info@azani.org>
2023-10-14 23:33:03 -0700

    arrays lab

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    return cats.push("Ralph");
+}
+
+function destructivelyPrependCat(name) {
+    return cats.unshift("Bob");
+}
+
+function destructivelyRemoveLastCat(name) {
+    return cats.pop();
+}
+
+function destructivelyRemoveFirstCat(name) {
+    return cats.shift();
+}
+
+function appendCat(name) {
+    const newCats = [...cats, "Broom"];
+    console.log(cats);
+    return newCats;
+}
+
+function prependCat(name) {
+    const oldCats = ["Arnold", ...cats]
+    console.log(cats);
+    return oldCats;
+}
+
+function removeLastCat(name) {
+    const twoCats = cats.slice(0,2);
+    return twoCats;
+}
+
+function removeFirstCat(name) {
+    const oneCat = cats.slice(1);
+    return oneCat;
+}
